219e4a3c2f4e6f5bb330c6ed93f24e3d5bf0b7d9
panglishan <1306573126@qq.com>
2018-11-07 14:39:15 +0800

    init

---
+++ src/GetuiServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Panglishan\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+
+class GetuiServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        if($this->app instanceof LumenApplication)
+        {
+            $this->app->configure('getui');
+        }
+
+        $this->mergeConfigFrom(__DIR__.'/config/getui.php','getui')
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton(GetuiService::class, function ($app) {
+            return new GetuiService($app->config->get('getui', []));
+        });
+        $this->app->alias(GetuiService::class, 'getui');
+    }
+}
